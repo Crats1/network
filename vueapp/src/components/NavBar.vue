08f1029f8c9310d14bda74fa487357bbf765e615
Crats1 <cstar1107@gmail.com>
2023-04-12 15:27:54 +1000

    Added bootstrap

---
+++ vueapp/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="#">Network</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="#">All Posts</a>
+                <a class="nav-item nav-link" href="#">Following</a>
+                <a class="nav-item nav-link" href="#">Profile</a>
+                <a class="nav-item nav-link" href="#">Login</a>
+                <a class="nav-item nav-link" href="#">Register</a>
+                <a class="nav-item nav-link" href="#">Log out</a>
+            </div>
+        </div>
+    </nav>
+
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+defineProps({
+
+});
+</script>
+
+<style scoped>
+</style>
